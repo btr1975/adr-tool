0e44755aab5787ec04b22114be3ecf9246cecd87
Ben Trachtenberg <btr1975@gmail.com>
2023-11-06 10:36:07 -0600

    Moved to version 1.0.1

--- internal/stuff.go
+++ internal/stuff.go
@@ -9,4 +9,5 @@ func main() {
 	now := time.Now()
 	fmt.Println(fmt.Sprintf("%v", now.Format("Mon Jan 2 15:04:05 MST 2006")))
 	fmt.Println(fmt.Sprintf("%v", now.Format("2006-01-02")))
+	fmt.Println(fmt.Sprintf("%v", now.Format("01/02/2006")))
 }
