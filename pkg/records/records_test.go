8fe36aabbc4a161d32c469daec7906531d556834
Ben Trachtenberg <btr1975@gmail.com>
2023-11-05 20:55:27 -0600

    added more testing

---
+++ pkg/records/records_test.go
@@ -0,0 +1,130 @@
+package records
+
+import (
+	"path"
+	"testing"
+)
+
+func TestStringToStatus(t *testing.T) {
+	scenarios := []struct {
+		name     string
+		input    string
+		expected Status
+		err      bool
+	}{
+		{
+			name:     "proposed",
+			input:    "proposed",
+			expected: Proposed,
+			err:      false,
+		},
+		{
+			name:     "accepted",
+			input:    "accepted",
+			expected: Accepted,
+			err:      false,
+		},
+		{
+			name:     "rejected",
+			input:    "rejected",
+			expected: Rejected,
+			err:      false,
+		},
+		{
+			name:     "deprecated",
+			input:    "deprecated",
+			expected: Deprecated,
+			err:      false,
+		},
+		{
+			name:     "superseded",
+			input:    "superseded",
+			expected: Superseded,
+			err:      false,
+		},
+		{
+			name:     "invalid",
+			input:    "invalid",
+			expected: "",
+			err:      true,
+		},
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			actual, err := StringToStatus(scenario.input)
+			if err != nil {
+				if !scenario.err {
+					t.Errorf("expected no error, got %v", err)
+				} else if scenario.err {
+					t.Logf("expected error, got %v", err)
+				}
+			}
+			if actual != scenario.expected {
+				t.Errorf("expected %s, got %s", scenario.expected, actual)
+			}
+		})
+	}
+}
+
+func TestFileExists(t *testing.T) {
+	goodPath := path.Join("..", "..", "test", "long", "my-title.md")
+	badPath := path.Join("..", "..", "test", "long", "my-title.txt")
+
+	scenarios := []struct {
+		name     string
+		input    string
+		expected bool
+	}{
+		{
+			name:     "file exists",
+			input:    goodPath,
+			expected: true,
+		},
+		{
+			name:     "file does not exist",
+			input:    badPath,
+			expected: false,
+		},
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			actual := FileExists(scenario.input)
+			if actual != scenario.expected {
+				t.Errorf("expected %t, got %t", scenario.expected, actual)
+			}
+		})
+	}
+}
+
+func TestDirectoryExists(t *testing.T) {
+	goodPath := path.Join("..", "..", "test", "long")
+	badPath := path.Join("..", "..", "test", "unknown")
+
+	scenarios := []struct {
+		name     string
+		input    string
+		expected bool
+	}{
+		{
+			name:     "directory exists",
+			input:    goodPath,
+			expected: true,
+		},
+		{
+			name:     "directory does not exist",
+			input:    badPath,
+			expected: false,
+		},
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			actual := DirectoryExists(scenario.input)
+			if actual != scenario.expected {
+				t.Errorf("expected %t, got %t", scenario.expected, actual)
+			}
+		})
+	}
+}
